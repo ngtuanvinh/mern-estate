96f869de3263d7e6322cd57eb042ef539102ebd9
Vinh Nguyen <phamnguyentuanvinhh@gmail.com>
2023-10-04 14:32:10 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("create the server on port 3000");
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("create the server on port 3000");
+    });
+  })
+  .catch((err) => console.log(err));
