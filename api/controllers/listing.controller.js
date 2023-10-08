72bc1260fdec58def749c5124858961fd6f153f9
Vinh Nguyen <phamnguyentuanvinhh@gmail.com>
2023-10-08 18:02:11 +0700

    add onChange and onSubmit functionality to the search page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -60,49 +60,50 @@ export const getListing = async (req, res, next) => {
 
 export const getListings = async (req, res, next) => {
   try {
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
 
-    if (offer === false || offer === undefined) {
-      offer = { $in: [true, false] };
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
 
-    if (furnished === false || furnished === undefined) {
-      furnished = { $in: [true, false] };
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
 
-    if (parking === false || parking === undefined) {
-      parking = { $in: [true, false] };
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
 
-    if (type === "all" || type === undefined) {
+    if (type === undefined || type === "all") {
       type = { $in: ["sale", "rent"] };
     }
 
-    const serchTerm = req.query.searchTerm || "";
+    const searchTerm = req.query.searchTerm || "";
+
     const sort = req.query.sort || "createdAt";
+
     const order = req.query.order || "desc";
 
     const listings = await Listing.find({
-      name: { $regex: serchTerm, $options: "i" },
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
       type,
     })
-      .sort({
-        [sort]: order,
-      })
+      .sort({ [sort]: order })
       .limit(limit)
       .skip(startIndex);
-    res.status(200).json(listings);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
